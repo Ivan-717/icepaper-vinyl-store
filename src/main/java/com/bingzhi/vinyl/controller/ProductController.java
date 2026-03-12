e2360d4bff6c494851dd3bf9e89f3da182b42797
Ivan-717 <2935728050@qq.com>
2026-03-12 23:38:15 +0800

    feat: 添加商品搜索接口

--- src/main/java/com/bingzhi/vinyl/controller/ProductController.java
+++ src/main/java/com/bingzhi/vinyl/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.bingzhi.vinyl.controller;
 import com.bingzhi.vinyl.entity.Product;
 import com.bingzhi.vinyl.mapper.ProductMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +22,16 @@ public class ProductController {
     public Product getById(@PathVariable Long id){
         return productMapper.findById(id);
     }
+
+    //搜索商品
+    @GetMapping("/search")
+    //@RequestParam会从URL?后面找keyword=周杰伦，把周杰伦赋值给keyword参数
+    public List<Product> search(@RequestParam String keyword){
+        return productMapper.search(keyword);
+    }
+
+
+
+
+
 }
--- src/main/java/com/bingzhi/vinyl/mapper/ProductMapper.java
+++ src/main/java/com/bingzhi/vinyl/mapper/ProductMapper.java
@@ -2,6 +2,7 @@ package com.bingzhi.vinyl.mapper;
 
 import com.bingzhi.vinyl.entity.Product;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -14,4 +15,11 @@ public interface ProductMapper {
 
     @Select("SELECT * FROM product WHERE id=#{id}")
     Product findById(Long id);
+
+    //根据关键词搜索
+    //LIKE "%love%" 的意思是：包含love的结果全都匹配
+
+    @Select("SELECT * FROM  product WHERE name LIKE CONCAT('%',#{keyword},'%') or artist LIKE CONCAT('%',#{keyword},'%')")
+    //@param("keyword")是把参数和sql里的#{。。。}联系起来
+    List<Product> search(@Param("keyword") String keyword);
 }
