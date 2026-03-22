faf5510259f0ed0dc83821fc971b1a9599fe7cee
Ivan-717 <2935728050@qq.com>
2026-03-22 20:38:52 +0800

    feat: 修复手机端访问的请求路径问题

--- src/main/java/com/bingzhi/vinyl/config/CorConfig.java
+++ src/main/java/com/bingzhi/vinyl/config/CorConfig.java
@@ -12,7 +12,12 @@ public class CorConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**") //对所有接口都生效
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:8080",       // 本地开发
+                        "http://localhost:8081",       // 本地 Nginx
+                        "http://10.118.188.109:8080",  // 你电脑的 IP + 后端端口
+                        "http://10.118.188.109:8081"   // 你电脑的 IP + Nginx 端口
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*") //允许前端传递请求头
                 .allowCredentials(true);  //允许携带凭证
